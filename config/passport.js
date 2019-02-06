53db71e04ef7dbaf8e1bb9a8234229df486fc462
Jose Roberto Quevedo <jose.quevedo2011@gmail.com>
2019-02-06 18:58:34 -0400

    missed where

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     },
     async (email, password, done) => {
       try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ where: { email }});
         if (!user) {
           return done(null, false, { message: "Incorrect username." });
         }
